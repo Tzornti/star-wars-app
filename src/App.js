afa3c3e0338c5c35e07117dde1423784839651df
Jordy <alejandre030902@estudiantec.cr>
2021-04-30 16:53:51 -0600

    Se agregaron cosas

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import React from 'react';
 
-export default App;
+export default class App extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state={
+      counter:0
+    }
+  }
+  componentDidMount() {
+    setInterval(()=> {
+      this.setState({
+        counter: this.state.counter + 1
+      });
+    },1000)
+  }
+
+  render() {
+    return (
+        <div className="App">
+          <header className="App-header">
+          <img src={logo}className={"App-logo"} alt={"logo"}/>
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target={"_blank"}
+              rel={"noopener noreferrer"}
+              >
+              learn React
+            </a>
+            <div>{this.state.counter}</div>
+          </header>
+        </div>
+    );
+  }
+}
